9437ea0b9467d7a46f8142faddad606a2e9e2783
Karen Liu <karen@patreon.com>
2015-11-22 17:46:01 -0800

    Port room-form to React.

--- index.js
+++ index.js
@@ -18927,13 +18927,51 @@ module.exports = require('./lib/React');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+function showUsersInRoom(roomName) {
+    window.currentRoomName = roomName;
+    window.currentRoomMembers = {};
+    $('#assign-button').hide();
+    $('#room-members').empty();
+    var usersRef = ref.child(roomName).child('users');
+    usersRef.on('child_added', function (userResponse) {
+        var userKey = userResponse.key();
+        var userName = userResponse.val();
+        window.currentRoomMembers[userKey] = userName;
+        $('#room-details').show();
+        $('#room-members').append('<span class="room-member">' + userName + '</span>');
+    });
+
+    ref.child(roomName).child('assignments').on('value', function (assignments) {
+        // If we have people in the room and we don't have assignments,
+        // show the assign button.
+        if (Object.keys(window.currentRoomMembers).length > 1 && !assignments.val()) {
+            $('#assign-button').show();
+        }
+
+        // If assignments exist, show the current user's assignment.
+        if (window.currentUserRef) {
+            var currentUserKey = window.currentUserRef.key();
+            var assignmentKey = assignments.val()[currentUserKey];
+
+            $('#assignment').show();
+            $('#assignment-name').text(window.currentRoomMembers[assignmentKey]);
+        }
+    });
+}
+
 var RoomForm = React.createClass({
     displayName: 'RoomForm',
+
+    handleSubmitRoom: function (e) {
+        e.preventDefault();
+        var roomName = $('#room-input').val();
+        showUsersInRoom(roomName);
+    },
 
     render: function () {
         return React.createElement(
             'form',
-            { id: 'room-form' },
+            { id: 'room-form', onSubmit: this.handleSubmitRoom },
             React.createElement(
                 'label',
                 null,
--- src/choose-room.js
+++ src/choose-room.js
@@ -1,10 +1,48 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+function showUsersInRoom(roomName) {
+    window.currentRoomName = roomName;
+    window.currentRoomMembers = {};
+    $('#assign-button').hide();
+    $('#room-members').empty();
+    var usersRef = ref.child(roomName).child('users')
+    usersRef.on('child_added', function(userResponse) {
+        var userKey = userResponse.key();
+        var userName = userResponse.val();
+        window.currentRoomMembers[userKey] = userName;
+        $('#room-details').show();
+        $('#room-members').append('<span class="room-member">' + userName + '</span>');
+    });
+
+    ref.child(roomName).child('assignments').on('value', function(assignments) {
+        // If we have people in the room and we don't have assignments,
+        // show the assign button.
+        if (Object.keys(window.currentRoomMembers).length > 1 && !assignments.val()) {
+            $('#assign-button').show();
+        }
+
+        // If assignments exist, show the current user's assignment.
+        if (window.currentUserRef) {
+            var currentUserKey = window.currentUserRef.key();
+            var assignmentKey = assignments.val()[currentUserKey];
+
+            $('#assignment').show();
+            $('#assignment-name').text(window.currentRoomMembers[assignmentKey]);
+        }
+    });
+}
+
 var RoomForm = React.createClass({
+    handleSubmitRoom: function(e) {
+        e.preventDefault();
+        var roomName = $('#room-input').val();
+        showUsersInRoom(roomName);
+    },
+
     render: function() {
         return (
-            <form id="room-form">
+            <form id="room-form" onSubmit={ this.handleSubmitRoom }>
                 <label>
                     <div><strong>What is the Secret Santa room name?</strong></div>
                     <input className="input" type="text" id="room-input" />
