561a00b29495b1d867388f2eb14e86fcff090a02
Karen Liu <karen@patreon.com>
2015-12-08 19:07:13 -0800

    Use a shuffle + rotate algorithm.

--- src/assign_button.js
+++ src/assign_button.js
@@ -11,16 +11,6 @@ function shuffleArray(array) {
     return array;
 }
 
-// Basically checks if anyone got themselves.
-function isValidShuffle(array1, array2) {
-    for (let i = 0; i < array1.length; i++) {
-        if (array1[i] === array2[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 module.exports = React.createClass({
     propTypes: {
         members: React.PropTypes.object.isRequired,
@@ -28,16 +18,14 @@ module.exports = React.createClass({
     },
 
     handleAssign: function() {
-        const originalUsers = Object.keys(this.props.members);
-        const shuffledUsers = originalUsers.slice(0);
-        do {
-            shuffleArray(shuffledUsers);
-        } while (!isValidShuffle(shuffledUsers, originalUsers));
-
+        const shuffledUsers = shuffleArray(Object.keys(this.props.members));
+        const numUsers = shuffledUsers.length;
         let newAssignments = {};
-        for (let i in shuffledUsers) {
-            newAssignments[originalUsers[i]] = shuffledUsers[i];
+        for (let i = 0; i < numUsers - 1; i++) {
+            newAssignments[shuffledUsers[i]] = shuffledUsers[i+1];
         }
+        newAssignments[shuffledUsers[numUsers - 1]] = shuffledUsers[0];
+
         this.props.handleAssign.call(null, newAssignments);
     },
 
