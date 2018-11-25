8fba6af06bd93d8d35c091adb894792e9813a749
Karen Liu <karenliu1@gmail.com>
2018-11-24 18:14:20 -0800

    Fix a React key warning.

--- src/room_member_list.js
+++ src/room_member_list.js
@@ -9,7 +9,7 @@ module.exports = React.createClass({
         const members = this.props.members;
         const roomMembersEl = Object.keys(members).map(function(memberKey) {
             return (
-                <span className="room-member" id={ memberKey } >
+                <span className="room-member" id={ memberKey } key={ memberKey }>
                     { members[memberKey] }
                 </span>
             );
